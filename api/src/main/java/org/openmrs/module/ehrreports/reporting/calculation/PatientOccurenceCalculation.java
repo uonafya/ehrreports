6da7b84cdb141569b8a42a656b5219af98e8dcc5
ningosi <nicotwendelee@gmail.com>
2020-05-08 13:02:18 +0300

    Added the encounters to the calculation

--- api/src/main/java/org/openmrs/module/ehrreports/reporting/calculation/PatientOccurenceCalculation.java
+++ api/src/main/java/org/openmrs/module/ehrreports/reporting/calculation/PatientOccurenceCalculation.java
@@ -1,13 +1,16 @@
 package org.openmrs.module.ehrreports.reporting.calculation;
 
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import org.openmrs.Encounter;
+import org.openmrs.EncounterType;
 import org.openmrs.api.context.Context;
 import org.openmrs.calculation.patient.PatientCalculationContext;
 import org.openmrs.calculation.result.CalculationResultMap;
 import org.openmrs.calculation.result.ListResult;
+import org.openmrs.module.ehrreports.metadata.OutpatientMetadata;
 import org.openmrs.module.ehrreports.reporting.utils.EhrCalculationUtils;
 import org.openmrs.module.ehrreports.reporting.utils.EhrReportConstants;
 import org.springframework.stereotype.Component;
@@ -22,22 +25,30 @@ public class PatientOccurenceCalculation extends AbstractPatientCalculation {
       PatientCalculationContext context) {
     EhrCalculationService ePTSCalculationService =
         Context.getRegisteredComponents(EhrCalculationService.class).get(0);
+    // External dependencies
+    OutpatientMetadata outpatientMetadata =
+        Context.getRegisteredComponents(OutpatientMetadata.class).get(0);
+    EncounterType regReturn = outpatientMetadata.getRegReturnEncounterType();
     CalculationResultMap resultMap = new CalculationResultMap();
     EhrReportConstants.OccurenceStates state =
         (EhrReportConstants.OccurenceStates) parameterValues.get("state");
 
     CalculationResultMap allEncounters =
-        ePTSCalculationService.allEncounters(null, cohort, null, context.getNow(), context);
+        ePTSCalculationService.allEncounters(
+            Arrays.asList(regReturn), cohort, null, context.getNow(), context);
     for (Integer pId : cohort) {
       boolean isCandidate = false;
       ListResult pregnantResult = (ListResult) allEncounters.get(pId);
       List<Encounter> encounterList = EhrCalculationUtils.extractResultValues(pregnantResult);
-
-      if (state.equals(EhrReportConstants.OccurenceStates.NEW) && encounterList.size() == 1) {
-        isCandidate = true;
-      } else if (state.equals(EhrReportConstants.OccurenceStates.REVISIT)
-          && encounterList.size() > 1) {
-        isCandidate = true;
+      for (Encounter e : encounterList) {
+        if (state.equals(EhrReportConstants.OccurenceStates.REVISIT)
+            && e.getEncounterType().equals(regReturn)) {
+          isCandidate = true;
+        }
+        if (state.equals(EhrReportConstants.OccurenceStates.NEW)
+            && !(e.getEncounterType().equals(regReturn))) {
+          isCandidate = true;
+        }
       }
       resultMap.put(pId, new BooleanResult(isCandidate, this));
     }
