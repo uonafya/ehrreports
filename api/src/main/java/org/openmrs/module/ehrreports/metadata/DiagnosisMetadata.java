103d9614aa24bea65c9cc1ae038acbe9f388dc21
injiri <simoninjiri@gmail.com>
2020-07-08 16:06:08 +0300

    TB concpts for pulling Tb concepts

--- api/src/main/java/org/openmrs/module/ehrreports/metadata/DiagnosisMetadata.java
+++ api/src/main/java/org/openmrs/module/ehrreports/metadata/DiagnosisMetadata.java
@@ -3,9 +3,12 @@ package org.openmrs.module.ehrreports.metadata;
 import java.util.Arrays;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class DiagnosisMetadata extends Metadata {
   @Autowired private OutpatientMetadata outpatientMetadata;
+
   /**
    * Get all diarrohea concepts as a list
    *
@@ -15,5 +18,31 @@ public class DiagnosisMetadata extends Metadata {
     return Arrays.asList(
         outpatientMetadata.getAcuteDiarrhoea().getConceptId(),
         outpatientMetadata.getDiarrheaUnspecified().getConceptId());
+  }
+
+  public List<Integer> getTBConceptList() {
+    return Arrays.asList(
+        outpatientMetadata.getTuberculosisConcept().getConceptId(),
+        outpatientMetadata.getTBOtherOrgansConcept().getConceptId(),
+        outpatientMetadata.getTBInPregnancyConcept().getConceptId(),
+        outpatientMetadata.getTBAffectingCentralNervousSystemConcept().getConceptId(),
+        outpatientMetadata.getTBOfTheGenitourinarySystemConcept().getConceptId(),
+        outpatientMetadata.getMiliaryTBConcept().getConceptId(),
+        outpatientMetadata.getPulmonaryTBConcept().getConceptId(),
+        outpatientMetadata.getSequelaeOfTBConcept().getConceptId(),
+        outpatientMetadata.getRespiratoryTBConcept().getConceptId(),
+        outpatientMetadata.getRespiratoryTBUnsuspectedConcept().getConceptId(),
+        outpatientMetadata.getTBHistologicallyConfirmedConcept().getConceptId(),
+        outpatientMetadata
+            .getRespiratoryTBNotConfirmedBacteriologicallyAndHistologicallyConcept()
+            .getConceptId(),
+        outpatientMetadata.getExtrapulmonaryTBConcept().getConceptId(),
+        outpatientMetadata.getAcuteMiliaryTBUspecifiedConcept().getConceptId(),
+        outpatientMetadata.getOtherRespiratoryTBConcept().getConceptId(),
+        outpatientMetadata.getMultiDrugResistantTBConcept().getConceptId(),
+        outpatientMetadata.getSputumPositivePulmonaryTB().getConceptId(),
+        outpatientMetadata.getSputumNegativeTBConcept().getConceptId(),
+        outpatientMetadata.getContactWithAndExposureToTB().getConceptId(),
+        outpatientMetadata.getEncounterForScreeningForRespiratoryTB().getConceptId());
   }
 }
